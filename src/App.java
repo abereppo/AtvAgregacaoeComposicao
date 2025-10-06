2547e72ae34bfda88d3e56e6281139a771fc2117
abereppo <88676950+abereppo@users.noreply.github.com>
2025-10-06 17:45:14 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Professor p1 = new Professor("Ana Silva");
+        Turma t = new Turma("ADS-101");
+
+        t.setProfessor(p1);
+        t.resumo();
+
+        Professor p2 = new Professor("Carlos Souza");
+        t.setProfessor(p2);
+        t.resumo(); 
     }
 }
